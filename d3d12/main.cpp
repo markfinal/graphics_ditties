458983f8887920eb2c2bfb3ebcd7bce295accba6
Mark Final <markfinal@hotmail.com>
2021-08-01 16:58:25 +0100

    Not at all obvious code to create a D3d12 device

--- d3d12/main.cpp
+++ d3d12/main.cpp
@@ -1,4 +1,16 @@
+#include <d3d12.h>
+#include <cassert>
+
 int main()
 {
+    HRESULT result;
+    ID3D12Device *device = nullptr;
+    result = D3D12CreateDevice(
+        nullptr,
+        D3D_FEATURE_LEVEL_9_3,
+        IID_PPV_ARGS(&device)
+    );
+    assert(S_OK == result);
+    // TODO: how to destroy device?
     return 0;
 }
