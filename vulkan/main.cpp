f70034efc858c380879e2ffa93820e7e013c45cb
Mark Final <markfinal@hotmail.com>
2021-08-03 21:21:15 +0100

    Vulkan headers

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -1,4 +1,33 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+static void error_callback(int code, const char *description)
+{
+    std::cerr << "glfw error code: " << code << " (" << description << ")" << std::endl;
+}
+
 int main()
 {
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+
+    std::cout << "Running against GLFW " << major << "." << minor << "." << revision << std::endl;
+
+    if (!glfwVulkanSupported())
+    {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwTerminate();
+
     return 0;
 }
