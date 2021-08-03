21873b5b60be67382af4a46f882bb0c4128ff8e5
Mark Final <markfinal@hotmail.com>
2021-08-03 18:57:29 +0100

    opengl: Using glfw as a window framework

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,4 +1,46 @@
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+static void error_callback(int code, const char *description)
+{
+    std::cerr << "glfw error code: " << code << " (" << description << ")" << std::endl;
+}
+
+static void render(GLFWwindow *window)
+{
+    glfwMakeContextCurrent(window);
+
+    glClearColor(1, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 int main()
 {
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    glfwSetErrorCallback(error_callback);
+
+    int major, minor, revision;
+    glfwGetVersion(&major, &minor, &revision);
+
+    std::cout << "Running against GLFW " << major << "." << minor << "." << revision << std::endl;
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "opengl", NULL, NULL);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        render(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
