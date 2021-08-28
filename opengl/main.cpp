c675e0ca9ceb5c23bf1be6a7a62527b9e410066c
Mark Final <markfinal@hotmail.com>
2021-08-28 21:17:25 +0100

    OpenGL: Fallback to OpenGL 3.3 if 4.1 isn't available

--- opengl/main.cpp
+++ opengl/main.cpp
@@ -1,8 +1,11 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 
+static int last_error = GLFW_NO_ERROR;
+
 static void error_callback(int code, const char *description)
 {
+    last_error = code;
     std::cerr << "glfw error code: " << code << " (" << description << ")" << std::endl;
 }
 
@@ -38,6 +41,13 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     GLFWwindow *window = glfwCreateWindow(640, 480, "OpenGL ditty", NULL, NULL);
+    if (nullptr == window && GLFW_VERSION_UNAVAILABLE == last_error)
+    {
+        // fall back if OpenGL 4 isn't available (e.g. in a VM)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        window = glfwCreateWindow(640, 480, "OpenGL ditty", NULL, NULL);
+    }
 
     while (!glfwWindowShouldClose(window))
     {
